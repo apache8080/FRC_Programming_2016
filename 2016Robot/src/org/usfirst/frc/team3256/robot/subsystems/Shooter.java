a5089d34683177298cd443862d1980ec3e3cc993
vcrobotics <aloha1epp@yahoo.com>
2016-02-10 17:42:42 -0800

    Defined Initial Methods for Intake and Shooter

--- 2016Robot/src/org/usfirst/frc/team3256/robot/subsystems/Shooter.java
+++ 2016Robot/src/org/usfirst/frc/team3256/robot/subsystems/Shooter.java
@@ -9,5 +9,31 @@ public class Shooter extends Subsystem{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	//pulls the catapult back
+	public void winchBack(){
+	
+	}
+	
+	//releases the winch and shoots the ball via pancake actuator
+	public void disEngageWinch(){
+		
+	}
+	
+	//engages the winch to be able to pull back and reload via pancake actuator
+	public void engageWinch (){
+
+	}
+	
+	//based on if the catapult is pulled back via limit switch
+	public boolean isWinched(){
+		return true;
+	}
+	
+	//based on if the ball is detected via IR Breaker
+	public boolean isLoaded(){
+		return true;
+	}
+	
 
 }
