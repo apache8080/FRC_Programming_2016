452d0d2489478418e1f225368a6dcbf7913eee39
vcrobotics <aloha1epp@yahoo.com>
2016-03-31 16:08:04 -0700

    GetPosition appears to be now working, need to test with PID on actual robot

---
+++ 2016DriveTest/src/org/usfirst/frc/team3256/robot/MotionProfileController.java
@@ -0,0 +1,116 @@
+package org.usfirst.frc.team3256.robot;
+
+import org.usfirst.frc.team3256.robot.Segment;
+
+public class MotionProfileController {
+
+	/**
+	* This is the MotionProfileController class that follows a pre-calculated trajectory.
+	*/
+
+
+	Segment[] trajectory;
+	double max_velocity;
+	double kV = 0.0;
+	double kA;
+	double kP;
+	double kI;
+	double kD;
+	double prev_error;
+	double sum_error;
+	boolean is_finished = false;
+	double pi = 3.141596535897932384626;
+	double ticksPerRotationHighGear = 2816;
+
+	public MotionProfileController(Segment[] trajectory,
+	double max_velocity,
+	double kA,
+	double kP,
+	double kI,
+	double kD){
+		this.trajectory = trajectory;
+		this.max_velocity = max_velocity;
+		this.kA = kA;
+		this.kP = kP;
+		this.kI = kI;
+		this.kD = kD;
+		this.prev_error = 0.0;
+		this.sum_error = 0.0;
+		this.kV = 1/max_velocity;
+
+	}
+	   
+	public double inchesToTicksHG(double distance){
+		   return (distance/(6*pi)*ticksPerRotationHighGear);
+	}
+
+	/**
+	* This method takes the velocity and acceleration value at this step and then calculates the motor output.
+	* @param   velocity (velocity of robot at this step)
+	* @param   acceleration (acceleration of robot at this step)
+	* @return  motor output
+	*/
+	public double calculateFeedForward(double velocity, double acceleration){
+		return (kV*velocity)+(kA*acceleration);
+	}
+
+	/**
+	* This method takes what the position should be at this step and what the robot position currently is then using PID calculates the new motor output.
+	* @param   setpoint (where the robot should be)
+	* @param   current (where the robot currently is)
+	* @return  motor output
+	*/
+	public double calculatePID(double setpoint, double current){
+		double setpoint_ticks = inchesToTicksHG((setpoint/12));
+		double error = (setpoint_ticks-current);
+		if (error < 0){
+			error = 0;
+			System.out.println("00000000000000000");
+		}
+		double P = kP*error;
+		sum_error += error;
+		double I = kI*sum_error;
+		double change_error = (prev_error-error);
+		double D = kD*change_error;
+		prev_error = error;
+		System.out.println("Setpoint_Ticks" + setpoint_ticks);
+		System.out.println("SetpointFeet" + setpoint);
+		System.out.println("Error" + error);
+		return (P+I+D);
+	}
+
+	/**
+	* This returns whether or not the robot is finished traveling the trajectory.
+	* @return is_finished (boolean value)
+	*/
+	public boolean isFinished(){
+		return is_finished;
+	}
+
+	/**
+	* This method is called every 20 miliseconds by the MoveForward command to step through the trajectory.
+	* @param   step (the spot the robot is at in the trajectory)
+	* @param   current (the current position of the robot based off of the encoders)
+	* @return  output (the motor value for the robot)
+	*/
+	public double getSpeed(int step, double current_position){
+		double output;
+		if(step >= trajectory.length){
+			is_finished=true;
+			output = 0.0;
+		}else{
+			Segment s = trajectory[step];
+			double velocity = s.getVelocity();
+			double acceleration = s.getAcceleration();
+			double position = s.getPosition();
+			//System.out.println(velocity + "\n");
+			double feed_forward = calculateFeedForward(velocity, acceleration);
+			double feedback = calculatePID(position, current_position);
+			System.out.println("Feedback" + feedback + "\n");
+			output = feed_forward+feedback;
+		}
+
+		return output;
+	}
+
+}
---
+++ 2016DriveTest/src/org/usfirst/frc/team3256/robot/TrajectoryGenerator.java
@@ -0,0 +1,153 @@
+package org.usfirst.frc.team3256.robot;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.usfirst.frc.team3256.robot.Segment;
+
+public class TrajectoryGenerator {
+
+	/**
+	* This is the TrajectoryGenerator class that creates the trajectory for a certain distance, velocity, and acceleration.
+	* The Trajectory is stored in an array.
+	*/
+
+
+	double max_v;
+	double max_accel;
+	double time_accel;
+	double time_deaccel;
+	double distance_total;
+	double distance_accel;
+	double distance_deaccel = 0.0;
+	double distance_cruise = 0.0;
+	double time_cruise = 0.0;
+	double time_total = 0.0;
+	double control_loop;
+
+	public TrajectoryGenerator(double distance_total,
+	double max_v, double max_accel,
+	double time_accel, double distance_accel,
+	double control_loop){
+		this.distance_total = distance_total;
+		this.max_v = max_v;
+		this.max_accel = max_accel;
+		this.time_accel = time_accel;
+		this.distance_accel = distance_accel;
+		this.control_loop = control_loop;
+		this.distance_deaccel = distance_accel;
+		this.distance_cruise = distance_total-distance_accel-distance_accel;
+		this.time_cruise = distance_cruise/max_v;
+		this.time_total = (time_cruise+(time_accel+time_accel));
+		this.time_deaccel = (time_total-time_accel);
+	}
+
+	/**
+	* This method returns the total time this movement should take
+	* @return time_total (total time to move)
+	*/
+	public double getTimeTotal(){
+		return time_total;
+	}
+
+	/**
+	* This method returns the max velocity of the robot.
+	* @return max_v (max velocity of robot)
+	*/
+	public double getMaxVelocity(){
+		return max_v;
+	}
+
+
+	/**
+	* This method calculates the velocity in ft/s at a specific time.
+	* @param  time (time in seconds based off of FPGA Timestamp)
+	* @return velocity (velocity of robot at time in ft/s)
+	*/
+	public double calculateV(double time){
+		double velocity = 0;
+
+		if (time<time_accel){
+			velocity = max_accel*time;
+		}
+		else if (time > time_accel && time < time_deaccel){
+			velocity = max_v;
+		}
+		else {
+			velocity = max_v-(max_accel*(time));
+		}
+		return velocity;
+	}
+
+	/**
+	* This method calculates the acceleration in ft/s^2 at a specific time.
+	* @param  time (time in seconds based off of FPGA Timestamp)
+	* @return acceleration (acceleration of robot at time in ft/s^2)
+	*/
+	public double calculateA(double time){
+		double acceleration = 0;
+		if (time<time_accel){
+			acceleration = max_accel;
+		}
+		else if (time > time_accel && time < time_deaccel){
+			acceleration = 0;
+		}
+		else {
+			acceleration = -max_accel;
+		}
+		return acceleration;
+
+	}
+
+	/**
+	* This method calculates the position of the robot in feet at a specific time.
+	* @param  time (time in seconds based off of FPGA Timestamp)
+	* @return positon (position of robot at time in feet)
+	*/
+	public double calculateS(double time){
+		double position = 0;
+		if (time<time_accel){
+			position = time*calculateV(time)/2;
+		}
+		else if (time>time_accel && time < time_deaccel){
+			position = time_accel*max_v/2 + (time-time_accel)*max_v;
+		}
+		else {
+			position = time_accel*max_v/2 + (time_deaccel-time_accel)*max_v + (time-time_deaccel)*-calculateV(time)/2;
+		}
+		
+		return position;
+	}
+
+	/**
+	* This function returns an array of segments that make up the Trajectory.
+	* @return trajectory (array of segments)
+	*/
+	public Segment[] getTrajectory(){
+		double time = 0.0;
+		List<Segment> trajectory = new ArrayList<Segment>();
+		while(time<=time_total){
+			double velocity = calculateV(time);
+			double acceleration = calculateA(time);
+			double position = calculateS(time);
+			System.out.println("Position " + position);
+			System.out.println("Velocity " + velocity);
+			System.out.println("Acceleration" + acceleration);
+			System.out.println("Time" + time);
+			System.out.println("distance_cruise " + distance_cruise);
+			System.out.println("distance_total " + distance_total);
+			System.out.println("distance_accel " + distance_accel);
+			System.out.println("time_total " + time_total);
+			System.out.println("time_cruise " + time_cruise);
+			System.out.println("time_accel " + time_accel);
+			System.out.println("time_deaccel " + time_deaccel);
+			System.out.println("           ");
+			Segment s = new Segment(time, velocity, acceleration, position);
+			trajectory.add(s);
+			time += (1/control_loop);
+		}
+		Segment[] trajectory_arr = new Segment[trajectory.size()];
+		trajectory.toArray(trajectory_arr);
+		return trajectory_arr;
+	}
+}
